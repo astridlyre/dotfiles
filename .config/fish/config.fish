89103e26251f8c4b3151f4111f259039855870cc
Astrid <astrid.lyre@outlook.com>
2020-12-30 14:38:27 -0800

    dotfiles v0.0.1

---
+++ .config/fish/config.fish
@@ -0,0 +1,64 @@
+# ~/.config/fish/config.fish
+
+# --------------------
+# Set variables
+# --------------------
+set -x EDITOR vim
+set fish_greeting
+
+# --------------------
+# Aliases
+# --------------------
+alias ls="exa -G --color auto --icons -a -s type"
+alias ll="exa -l --color always --icons -a -s type"
+alias cat="batcat -pp --theme=base16"
+alias fd="fdfind"
+alias vi="nvim"
+alias vim="nvim"
+alias c="clear"
+alias x="exit"
+alias python="python3"
+alias update="sudo apt update && sudo apt upgrade"
+alias chx="chmod +x"
+alias ducks="du -sh * | sort -hr | head -11"
+
+# --------------------
+# Navigation
+# --------------------
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+# --------------------
+# Functions
+# --------------------
+
+# btc: Get bitcoin price "@30d"
+function btc
+	curl rate.sx/btc$argv\?qF
+end
+
+# market: Top n crypto coins
+function market
+	curl rate.sx/\?qFn=$argv
+end
+
+# weather: Get weather in city 
+function weather
+	curl wttr.in/$argv\?qf
+end
+
+# cht: Find doc of cmd
+function cht
+	curl cht.sh/$argv
+end
+
+# md: Make dir and cd to it
+function md
+	mkdir -p $argv && cd $argv
+end
+
+# Enable vi mode
+function fish_user_key_bindings
+  fish_vi_key_bindings
+end
