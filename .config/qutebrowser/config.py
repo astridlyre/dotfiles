7b4b782af4294ee8d43d5f88bb32be344a22ad28
Erin Burton <eb@flat9.dev>
2021-04-17 17:43:25 -0700

    qutebrowser config Saturday, April 17, 2021, 5:43:25 PM

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,11 @@
+import moonlight.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+moonlight.draw.blood(c, {
+    'spacing': {
+        'vertical': 2,
+        'horizontal': 4
+    }
+})
