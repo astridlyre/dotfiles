4acd15d4d7e22ca64a649762b01e8384e54b791a
Erin Burton <eb@flat9.dev>
2021-12-30 05:49:05 -0800

    dots Thu December 30 2021 @ 05:49:05

--- .config/nvim/lua/user/plugins/coq.lua
+++ .config/nvim/lua/user/plugins/coq.lua
@@ -36,7 +36,7 @@ vim.g.coq_settings = {
 		lsp = {
 			enabled = true,
 			weight_adjust = 2.0,
-			resolve_timeout = 0.1,
+			resolve_timeout = 0.09,
 		},
 		snippets = {
 			enabled = true,
@@ -45,7 +45,7 @@ vim.g.coq_settings = {
 	},
 	auto_start = "shut-up",
 	limits = {
-		completion_auto_timeout = 0.2,
+		completion_auto_timeout = 0.17,
 	},
 }
 remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
